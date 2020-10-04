696901753e6a5fb625a5cd4032ec7521f864f537
ulises-jeremias <ulisescf.24@gmail.com>
2020-10-04 16:30:17 -0300

    Updated defs

--- src/gRPC/user_lookup/client/main.go
+++ src/gRPC/user_lookup/client/main.go
@@ -18,17 +18,16 @@ func main() {
 	c := user_lookup.NewOperationsClient(conn)
 	username := &user_lookup.Username{Name: "root"}
 
-	var userID, err = c.ByUsername(context.Background(), username)
+	userID, err := c.ByUsername(context.Background(), username)
 	if err != nil {
 		log.Fatalf("Error when calling ByUsername: %s", err)
 	}
 	log.Printf("El ID de root es: %s", userID.Num)
 
 	userID = &user_lookup.UserID{Num: "0"}
-	username2, err := c.ById(context.Background(), userID)
+	username, err = c.ByID(context.Background(), userID)
 	if err != nil {
 		log.Fatalf("Error when calling Sub: %s", err)
 	}
-	log.Printf("El username del Id 0 es: %s", username2.Name)
-
+	log.Printf("El username del Id 0 es: %s", username.Name)
 }
